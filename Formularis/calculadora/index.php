d5684e6ecf13b0cfb5fd5a439301547c431bcf37
ntorrent5 <nyly05@gmail.com>
2022-10-03 16:21:56 +0200

    versió correcte

--- Formularis/calculadora/index.php
+++ Formularis/calculadora/index.php
@@ -8,21 +8,22 @@
 <?php
 
 function mostrarNumero(){
-    $valor = implode($_GET);
+    $valor = implode($_POST);
     if (isset($_REQUEST["resultat"]) && (str_contains($_REQUEST['resultat'], "INF")) || (str_contains($_REQUEST['resultat'], "ERROR"))) {
 
         $valor = str_replace("INF","",$valor);
         $valor = str_replace("ERROR","",$valor);
     }
    
-    switch(end($_GET)){
+    switch(end($_POST)){
         case "C":
             $valor = ""; 
             break;
         case "=":
             try{
-                array_pop($_GET);
-                eval("$valor= (".implode($_GET).");");
+                array_pop($_POST);
+                preg_match('/(0-9)*}', 'ac', $matches);
+                eval("$valor= (".implode($_POST).");");
                 if(is_float($valor) && (int)strlen(substr(strrchr($valor, "."), 1)) > 4 ){
                     $valor = number_format((float)$valor, 4, '.', '');
                 }
@@ -40,9 +41,13 @@ function mostrarNumero(){
 ?>
 <body>
     <div class="container">
-        <form name="calc" class="calculator" action="index.php" method="get">
-            <input type="text" class="value" readonly value= "<?= mostrarNumero()?>"  name="resultat"/>
-            <span class="num clear"><input type ="submit" value="C" name="tecla"></span>
+        <form name="calc" class="calculator" action="" method="post">
+            <input type="text" class="value" readonly value="<?= mostrarNumero()?>" name="resultat" />
+            <span class="num"><input type ="submit" value="(" name="tecla"></span>
+            <span class="num"><input type ="submit" value=")" name="tecla"></span>
+            <span class="num"><input type ="submit" value="SIN" name="tecla"></span>
+            <span class="num"><input type ="submit" value="COS" name="tecla"></span>
+            <span class="num clear" ><input type ="submit" value="C"name="tecla"></span>
             <span class="num"><input type ="submit" value="/" name="tecla"></span>
             <span class="num"><input type ="submit" value="*" name="tecla"></span>
             <span class="num"><input type ="submit" value="7" name="tecla"></span>
@@ -53,13 +58,13 @@ function mostrarNumero(){
             <span class="num"><input type ="submit" value="5" name="tecla"></span>
             <span class="num"><input type ="submit" value="6" name="tecla"></span>
             <span class="num plus"><input type ="submit" value="+" name="tecla"></span>
-            <span class="num"><input type ="submit" value="1" name="tecla"></span>
-            <span class="num"><input type ="submit" value="2" name="tecla"></span>
-            <span class="num"><input type ="submit" value="3" name="tecla"></span>
-            <span class="num"><input type ="submit" value="0" name="tecla"></span>
-            <span class="num"><input type ="submit" value="00" name="tecla"></span>
-            <span class="num"><input type ="submit" value="." name="tecla"></span>
-            <span class="num equal"><input type ="submit" value="=" name="tecla"></span>
+            <span class="num"><input type ="submit" value="1"name="tecla"> </span>
+            <span class="num"><input type ="submit" value="2"name="tecla"></span>
+            <span class="num"><input type ="submit" value="3"name="tecla"></span>
+            <span class="num"><input type ="submit" value="0"name="tecla"></span>
+            <span class="num"><input type ="submit" value="00"name="tecla"></span>
+            <span class="num"><input type ="submit" value="."name="tecla"></span>
+            <span class="num equal"><input type ="submit" value="="name="tecla"></span>
         </form>
     </div>
 </body>
