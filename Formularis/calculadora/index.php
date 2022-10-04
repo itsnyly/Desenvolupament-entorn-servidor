79871b779e839eb84192c60e93750efd90b7490a
ntorrent5 <nyly05@gmail.com>
2022-10-04 19:37:49 +0200

    entrega final calculadora

--- Formularis/calculadora/index.php
+++ Formularis/calculadora/index.php
@@ -8,12 +8,22 @@
 
 <?php
 
+/** 
+ * Funció principal. S'encarrega de mostrar per pantalla les tecles seleccionades.
+ * @return valor resultat que es mostra per pantalla.
+*/
 function mostarNumero() {
+
     $valor = implode($_POST);
+
     if (isset($_POST["resultat"]) && ($_POST["resultat"] == "ERROR" || $_POST["resultat"] == "INF")) {
         $valor = str_replace("INF","",$valor);
         $valor = str_replace("ERROR","",$valor);
     }
+    else{
+        $valor = comprovarOperacio($valor);
+    }
+
     switch (end($_POST)) {
         case 'Sin':
             $valor = '';
@@ -24,38 +34,67 @@ function mostarNumero() {
         case '=':
             array_pop($_POST);
             $valor = calcular(implode($_POST));
-            
             break;    
     }
-     return $valor;
-  }
-  
-  function calcular($operacio) {
 
+     return $valor;
+
+  }
+
+  /**
+   * Funció la qual comprova la cadena d'operació a executar per evitar que s'introdueixin 
+   * dos símbols d'operacions iguals de manera simultània.
+   * @param operacio string
+   * @return operacioPartida string
+   */
+
+  function comprovarOperacio($operacio){
+
+    $operacioPartida = str_split($operacio);
+    for ($i=0; $i < sizeof($operacioPartida)-1; $i++) { 
+       if($operacioPartida[$i] == $operacioPartida[$i +1] && !is_numeric($operacioPartida[$i]) && $operacioPartida[$i] != '(' && $operacioPartida[$i] != ')' && !preg_match('/[A-Za-z]/', $operacioPartida[$i])){
+            array_pop($operacioPartida);
+            return implode($operacioPartida);
+       }
+    }
+    return implode($operacioPartida);
+}
+
+  /**
+   * Executa la cadena ,passada per paràmetres, de manera literal per tal d'obtenir el resultat de la operació.
+   * Possa un màxim de 4 decimals de les operacions amb fracció decimal.
+   * Primerament recull l'excepció causada al dividir per zero i llavors recull els altre errors a 
+   * través d'una excepció global. 
+   * * Si s'intenta introduir codi php et mostrarà ERROR per pantalla.
+   * @param operació string
+   * @return valor resultat de la operació
+   */
+
+  function calcular($operacio) {
     try {
         if(preg_match('/^[0-9()+.\-*\(SIN)(COS)\/]+$/', $operacio)){
                     
             $valor = eval("return (".$operacio.");");
+            if(is_float($valor) && (int)strlen(substr(strrchr($valor, "."), 1)) > 4 ){
+                $valor = number_format((float)$valor, 4, '.', '');
+            }
+
         }else{
             $valor = "ERROR";
         }
         
-        if(is_float($valor) && (int)strlen(substr(strrchr($valor, "."), 1)) > 4 ){
-            $valor = number_format((float)$valor, 4, '.', '');
-        }
-    } catch (DivisionByZeroError $e) {
+    } catch (DivisionByZeroError $ex) {
 
         $valor = "INF";
 
-    } catch (Throwable $e) {
+    } catch (Throwable $ex) {
 
         $valor = "ERROR";
     }
-
     return $valor;
 
 }
- 
+
 ?>
 <body>
     <div class="container">
