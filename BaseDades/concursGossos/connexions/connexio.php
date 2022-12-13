bc34e5fd25fad97cc6497763d4b3d4b99aeae56d
itsNyly <ntorrent8@boscdelacoma.cat>
2022-12-13 16:28:12 +0100

    solucionar problema base de dades

--- BaseDades/concursGossos/connexions/connexio.php
+++ BaseDades/concursGossos/connexions/connexio.php
@@ -5,8 +5,8 @@ function connexio(){
     //  prova d'executar el contingut del try
     //  si falla executa el catch
     try {
-        $connexio = new baseDades("localhost","dwes-niltorrent-concurs-gossos","root","");
-        $pdo = new PDO ("mysql:host=$connexio->hostname;dbname=$connexio->dbname","$connexio->username");
+        $connexio = new baseDades("localhost","dwes-niltorrent-concurs-gossos","nil","Patata123");
+        $pdo = new PDO ("mysql:host=$connexio->hostname;dbname=$connexio->dbname","$connexio->username","$connexio->pw");
         return $pdo;
     } catch (PDOException $e) {
         return false;
